61f6d3925f15bd0ead588d0f1855e86ee588370b
penicillin0 <ayahitonakamura@gmail.com>
2021-10-26 20:55:24 +0900

    copy from origin

--- deploy/update-function-code.sh
+++ deploy/update-function-code.sh
@@ -3,8 +3,14 @@ cd ../src
 if [ -f ./lambda.zip ]; then
   rm ./lambda.zip
 fi
+
+if [ -f ./core30.json ]; then
+  rm ./core30.json
+fi
+
+cp ../data/core30.json ./
 zip -r lambda.zip .
 aws lambda update-function-code \
 --zip-file fileb://./lambda.zip \
 --function-name stock-core30-twitter-bot
-rm lambda.zip
+rm lambda.zip core30.json
